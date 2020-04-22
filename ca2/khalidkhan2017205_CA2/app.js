16d165c3729db62575823fb8b599b5cfdde1d590
KhalidKhan <khalid.khan@msn.com>
2020-04-22 13:44:56 +0000

    create,style landing.ejs and new.ejs. add get post  routes

--- ca2/khalidkhan2017205_CA2/app.js
+++ ca2/khalidkhan2017205_CA2/app.js
@@ -6,7 +6,7 @@ var express = require("express"),
 var app = express();
 
   // create and connect to database
-mongoose.connect("mongodb+srv://khalid:pass2017205@mymongodbcluster-f4oa8.mongodb.net/test?retryWrites=true&w=majority" , {
+mongoose.connect("mongodb+srv://khalid:pass2017205@mymongodbcluster-f4oa8.mongodb.net/studentDB?retryWrites=true&w=majority" , {
 		useNewUrlParser: true,
 		useCreateIndex: true,
         useUnifiedTopology: true,
@@ -21,11 +21,42 @@ app.set("view engine" , "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get("/" , function(req,res){
-    res.render("index");
+// this redirects us to students route
+app.get("/", function(req,res){
+    res.render("landing");
+});
+
+// this take us to index.ejs page
+app.get("/students" , function(req, res){
+    StdDB.find({}, function(err, myData){
+        if(err){
+            console.log("ERROR : " + err);
+        }else{
+            res.render("index", {data:myData} );
+        }
+    })
+});
+
+// new route == it will take us to new.ejs and dislay a form to fill
+app.get("/students/new" , function(req,res){
+    res.render("new");
+});
+// create route == we will create new student record and redirect it to /students
+app.post("/students", function(req,res){
+    
+    StdDB.create(req.body.student, function(err, newStudent){
+        if(err){
+            res.render("new");
+        }else{
+            res.redirect("/students");
+        }
+    });
 });
 
 
+
+
+// ***************************
 app.listen(3000, function(){
     console.log("SERVER RUNNING");
 })
