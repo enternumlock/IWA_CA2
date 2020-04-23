b73091bbbf75218150583944bc4f5ef583040c1b
KhalidKhan <khalid.khan@msn.com>
2020-04-23 21:13:43 +0000

    Refactor app.js and routes folder

--- khalidkhan2017205_CA2/app.js
+++ khalidkhan2017205_CA2/app.js
@@ -6,6 +6,9 @@ var express = require("express"),
 
 var app = express();
 
+//requring routes
+var studentRoutes    = require("./routes/students");
+
   // create and connect to database
 mongoose.connect("mongodb+srv://khalid:pass2017205@mymongodbcluster-f4oa8.mongodb.net/studentDB?retryWrites=true&w=majority" , {
 		useNewUrlParser: true,
@@ -27,81 +30,8 @@ app.use(methodOverride("_method"));
 app.get("/", function(req,res){
     res.render("landing");
 });
-
-// this take us to index.ejs page
-app.get("/students" , function(req, res){
-    StdDB.find({}, function(err, myData){
-        if(err){
-            console.log("ERROR : " + err);
-        }else{
-            res.render("index", {data:myData} );
-        }
-    })
-});
-
-// new route == it will take us to new.ejs and dislay a form to fill
-app.get("/students/new" , function(req,res){
-    res.render("new");
-});
-// create route == we will create new student record and redirect it to /students
-app.post("/students", function(req,res){
-    
-    StdDB.create(req.body.student, function(err){
-        if(err){
-            res.render("new");
-        }else{
-            res.redirect("/students");
-        }
-    });
-});
-
-// Show route == it will take us to show template
-app.get("/students/:id" , function(req,res){
-    // res.send("SHOW PAGE")
-    StdDB.findById(req.params.id, function(err, foundstudent){
-        if(err){
-            res.redirect("/students");
-        }else{
-            res.render("show", {data: foundstudent});
-        }
-    });
-});
-
-// =========================================>
-// Edit route == it will take us to Edit form
-app.get("/students/:id/edit", function(req,res){
-    // first find id of student
-    StdDB.findById(req.params.id, function(err, foundstudent){
-        if(err){
-            res.redirect("/students");
-        }else{
-            res.render("edit", {data: foundstudent});
-        }
-    });
-});
-
-// update route
-app.put("/students/:id", function(req,res){
-
-    StdDB.findByIdAndUpdate(req.params.id, req.body.student, function(err, updatedStudent){
-        if(err){
-            res.redirect("/students");
-        }else{
-            res.redirect("/students/" + req.params.id);
-        }
-    });
-});
-
-// Delete route
-app.delete("/students/:id", function(req,res){
-    StdDB.findByIdAndRemove(req.params.id , function(err){
-        if(err){
-            res.redirect("/students");
-        }else{
-            res.redirect("/students");
-        }
-    });
-});
+// use routes
+app.use(studentRoutes);
 
 app.listen(3000, function(){
     console.log("SERVER RUNNING");
---
+++ khalidkhan2017205_CA2/routes/students.js
@@ -0,0 +1,81 @@
+var express = require("express");
+var router  = express.Router();
+var StdDB = require("../models/studentDB");
+
+// Routes
+
+// this take us to index.ejs page
+router.get("/students" , function(req, res){
+    StdDB.find({}, function(err, myData){
+        if(err){
+            console.log("ERROR : " + err);
+        }else{
+            res.render("index", {data:myData} );
+        }
+    })
+});
+
+// new route == it will take us to new.ejs and dislay a form to fill
+router.get("/students/new" , function(req,res){
+    res.render("new");
+});
+// create route == we will create new student record and redirect it to /students
+router.post("/students", function(req,res){
+    // req.body.student.body = req.sanitize(req.body.student.body);
+    StdDB.create(req.body.student, function(err, newStudent){
+        if(err){
+            res.render("new");
+        }else{
+            res.redirect("/students");
+        }
+    });
+});
+
+// Show route == it will take us to show template
+router.get("/students/:id" , function(req,res){
+    // res.send("SHOW PAGE")
+    StdDB.findById(req.params.id, function(err, foundstudent){
+        if(err){
+            res.redirect("/students");
+        }else{
+            res.render("show", {data: foundstudent});
+        }
+    });
+});
+// =========================================>
+// Edit route == it will take us to Edit form
+router.get("/students/:id/edit", function(req,res){
+    // first find id of student
+    StdDB.findById(req.params.id, function(err, foundstudent){
+        if(err){
+            res.redirect("/students");
+        }else{
+            res.render("edit", {data: foundstudent});
+        }
+    });
+});
+
+// update route
+router.put("/students/:id", function(req,res){
+    // req.body.student.body = req.sanitize(req.body.student.body);
+    StdDB.findByIdAndUpdate(req.params.id, req.body.student, function(err, updatedStudent){
+        if(err){
+            res.redirect("/students");
+        }else{
+            res.redirect("/students/" + req.params.id);
+        }
+    });
+});
+
+// Delete route
+router.delete("/students/:id", function(req,res){
+    StdDB.findByIdAndRemove(req.params.id , function(err){
+        if(err){
+            res.redirect("/students");
+        }else{
+            res.redirect("/students");
+        }
+    });
+});
+
+module.exports = router;
